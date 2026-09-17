4135b316d7acc6d995e257e91c7d4bc688c4fceb
Ziyad Mouchfiq <zmouchfiq@gmail.com>
2026-09-17 11:37:51 +0000

    TP_regex_001

--- Ziyad Mouchfiq/TP-001.py
+++ Ziyad Mouchfiq/TP-001.py
@@ -1,21 +1,4 @@
-import re
-
-dataset = [
-    "Bonjour, mon email est   omar.k9999@example.com   et mon tel est 0612345678.",
-    "Contactez-nous via www.monsite.ma ou https://monsite.ma/contact !!",
-    "Le prix est 250 MAD le 17/09/2026, livraison rapide #IA @client_service",
-    "<p>Merci pour votre commande</p>   <br> Numero: +212 6-61-22-33-44",
-    "  Service   nul...    remboursement demande le 05-08-2026  , prix 1200,50 MAD ",
-    "Great service! Contact: sales@company.com or call 0522-334455 #satisfied",
-    "Facture N° INV-2026-00457 total: 3999.99 MAD TVA 20% date: 2026/09/01",
-    "Aucun probleme, tout est parfait :) suivez-nous @companyOfficial https://x.com/companyOfficial"
-]
-
- # email : partie_local @ domaine . extension   
-
-email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-
- #tele : indicateur c'est +212 ou 0 suivi de 9 chiffres Numero: +212 6-61-22-33-44
+ chiffres Numero: +212 6-61-22-33-44
 phone_pattern = r'(?:\+212|0)[\s\-]?[5-7](?:[\s\-]?\d){8}'
 
 # date JJ/MM/AAAA ou AAAA/MM/JJ ou JJ-MM-AAAA ou AAAA-MM-JJ
@@ -51,3 +34,44 @@ def clean_text(text):
 for text in dataset:
     cleaned_text = clean_text(text)
     print(cleaned_text)
+
+def extract_entities(text):
+    emails = re.findall(email_pattern, text)
+    phones = re.findall(phone_pattern, text)
+    dates = re.findall(date_pattern, text)
+    prices = re.findall(prix_pattern, text)
+    hashtags = re.findall(hashtag_pattern, text)
+    mentions = re.findall(mention_pattern, text)
+    urls = re.findall(url_pattern, text)
+    
+    return {
+        'emails': emails,
+        'phones': phones,
+        'dates': dates,
+        'prices': prices,
+        'hashtags': hashtags,
+        'mentions': mentions,
+        'urls': urls
+    }
+#"creat a dataframe to store the extracted entities"
+
+df = pd.DataFrame(columns=['original_text', 'emails', 'phones', 'dates', 'prices', 'hashtags', 'mentions', 'urls'])
+lignes = []
+for text in dataset:
+    entities = extract_entities(text)
+    ligne = {
+        'original_text': text,
+        'emails': entities['emails'],
+        'phones': entities['phones'],
+        'dates': entities['dates'],
+        'prices': entities['prices'],
+        'hashtags': entities['hashtags'],
+        'mentions': entities['mentions'],
+        'urls': entities['urls']
+    }
+    lignes.append(ligne)
+df = pd.DataFrame(lignes)
+print(df.to_string())
+#data= pd.DataFrame([extract_entities(text) for text in dataset])
+# data.insert(0, 'original_text', dataset)
+# print(data.to_string())
