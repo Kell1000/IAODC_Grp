322f3d19ebb0de298b711be5d2db6913145c9fa4
balkissawaberi-svg <balkissawaberi@gmail.com>
2026-09-23 17:50:06 +0000

    modifification

--- Balkissa_Waberi/TP2.py
+++ Balkissa_Waberi/TP2.py
@@ -11,17 +11,17 @@ def download_file(download_url, filename):
     file.close()
 
 for n_page in range(1, nb_pages):
-    url_i = 'https://core.ac.uk/search.....' 
+    url_i = 'https://core.ac.uk/search/?q=EARNING&page=' 
     url_page = url_i + str(n_page)
-    print('HTTP GET: %S' url_page)
+    print('HTTP GET: %S', url_page)
     response = requests.get(url_page)
     # parse content
     content = BeautifulSoup(response.text, 'lxml')
     # extract urls referencing PDF documents
     all_urls = content.find_all('a', href=True)
     # loop over all urls
-    for url in all_urls:
-        try:
+for url in all_urls:
+    try:
             if 'pdf' in url['href']:
                 #init pdf url
                 pdf_url=''
@@ -38,8 +38,8 @@ for n_page in range(1, nb_pages):
                 pdf_response = requests.get(pdf_url)
                 
                 # extract pdf file name
-                filename = urllib.request.unquote(pdf_response.url)...
+                filename = urllib.request.unquote(pdf_response.url)
                 download_file(url_path, filename)
 
-except Exception:
-pass
+    except Exception:
+         pass
